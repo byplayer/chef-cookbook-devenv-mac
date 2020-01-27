2d720ac55a7e44e110ea8ca3470ad4c029915152
byplayer <byplayer100@gmail.com>
2020-01-28 00:28:41 +0900

    :+1: Add homebrew
    Add homebrew cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,15 @@
 name 'devenv-mac'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'byplayer'
+maintainer_email 'byplayer100@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures devenv-mac'
 long_description 'Installs/Configures devenv-mac'
 version '0.1.0'
 chef_version '>= 14.0'
+
+%w[homebrew].each do |cookbook|
+  depends cookbook
+end
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+%w[homebrew].each do |cookbook|
+  include_recipe cookbook
+end
