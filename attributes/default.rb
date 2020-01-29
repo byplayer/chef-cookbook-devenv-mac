09b78bf08fb493584eec7e472d31826156b236ef
byplayer <byplayer100@gmail.com>
2020-01-29 13:21:05 +0900

    :+1: add tools repo
    Add tools to checkout

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,31 @@
 default['homebrew']['owner'] = 'brew_owner'
 default['homebrew']['formulas'] = %w(binutils git)
 default['homebrew']['casks'] = %w(emacs)
+
+default['devenv']['user'] = 'user'
+default['devenv']['group'] = 'group'
+
+default['devenv']['.zsh.d']['repo'] = 'https://github.com/byplayer/dot.zsh.d.git'
+default['devenv']['.zsh.d']['ref'] = 'master'
+default['devenv']['.zsh.d']['checkout_branch'] = nil
+default['devenv']['.zsh.d']['enable_checkout'] = false
+
+default['devenv']['.emacs.d']['repo'] = 'https://github.com/byplayer/dot.emacs.d.git'
+default['devenv']['.emacs.d']['ref'] = 'master'
+default['devenv']['.emacs.d']['checkout_branch'] = nil
+default['devenv']['.emacs.d']['enable_checkout'] = false
+
+default['devenv']['.git-extensions']['repo'] = 'https://github.com/muratayusuke/git-extensions.git'
+default['devenv']['.git-extensions']['ref'] = 'master'
+default['devenv']['.git-extensions']['checkout_branch'] = nil
+default['devenv']['.git-extensions']['enable_checkout'] = false
+
+default['devenv']['.ruby_tool']['repo'] = 'https://github.com/byplayer/ruby_tool.git'
+default['devenv']['.ruby_tool']['ref'] = 'master'
+default['devenv']['.ruby_tool']['checkout_branch'] = nil
+default['devenv']['.ruby_tool']['enable_checkout'] = false
+
+default['devenv']['.highlight']['repo'] = 'https://github.com/byplayer/dot.highlight.git'
+default['devenv']['.highlight']['ref'] = 'master'
+default['devenv']['.highlight']['checkout_branch'] = nil
+default['devenv']['.highlight']['enable_checkout'] = false
---
+++ recipes/_devenv.rb
@@ -0,0 +1,17 @@
+%w[.zsh.d
+   .emacs.d
+   .git-extensions
+   .ruby_tool
+   .highlight].each do |name|
+  git "#{devenv_user_home}/#{name}" do
+    repository node['devenv'][name]['repo']
+    reference node['devenv'][name]['ref']
+    user node['devenv']['user']
+    group node['devenv']['group']
+    checkout_branch node['devenv'][name]['checkout_branch']
+    enable_checkout node['devenv'][name]['enable_checkout']
+    enable_submodules true
+    action :sync
+  end
+end
+
