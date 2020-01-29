77e3aa7c90ff723514f541fc5d21ce4b56818df9
byplayer <byplayer100@gmail.com>
2020-01-29 15:17:24 +0900

    :bug: Fix homebrew formulas install issue
    Remove unnecessary packangename 'install'.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['homebrew']['owner'] = 'brew_owner'
 default['homebrew']['formulas'] =
   %w(binutils cmake cmigemo coreutils diffutils findutils fzf
-     git gpg highlight install pinentry-mac the_silver_searcher rbenv)
+     git gpg highlight pinentry-mac the_silver_searcher rbenv)
 default['homebrew']['casks'] = %w(emacs)
 
 default['devenv']['user'] = 'user'
