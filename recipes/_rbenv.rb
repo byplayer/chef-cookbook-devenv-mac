b14d373d58583303490e969b08b3c08363d3366e
byplayer <byplayer100@gmail.com>
2020-01-29 16:10:38 +0900

    :+1: Fix compile error

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -16,7 +16,7 @@ node['rbenv']['rubies'].each do |version, gems|
   end
 
   bash "install ruby(#{version}) by rbenv" do
-    environment {'HOME' => "/Users/#{node['rbenv']['user']}"}
+    environment({'HOME' => "/Users/#{node['rbenv']['user']}"})
     user node['rbenv']['user']
     code cmd
   end
