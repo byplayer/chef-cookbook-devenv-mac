53b46ea746c47e9f1bca9da782e2d1d726638bca
byplayer <byplayer100@gmail.com>
2020-02-18 13:12:08 +0900

    :+1: Add /Library to exclude path of updatedb
    Exclude /Library from updatedb to avoid unnecessary file to match

--- attributes/default.rb
+++ attributes/default.rb
@@ -79,7 +79,7 @@ default['eblook']['install_dir'] = '/opt/eblook'
 # updatedb
 default['updatedb']['prune_bind_mounts'] = 'yes'
 default['updatedb']['prunenames'] = '.git .bzr .hg .svn'
-default['updatedb']['prunepaths'] = '/tmp /private/tmp /var/spool /Applications'
+default['updatedb']['prunepaths'] = '/tmp /private/tmp /var/spool /Applications /Library'
 default['updatedb']['prunefs'] = 'NFS nfs nfs4 rpc_pipefs afs binfmt_misc proc smbfs autofs iso9660 ncpfs coda devpts ftpfs devfs mfs shfs sysfs cifs lustre_lite tmpfs usbfs udf fuse.glusterfs fuse.sshfs curlftpfs ecryptfs fusesmb devtmpfs'
 
 # golang
