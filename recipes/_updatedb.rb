5ec94ea0b6c50fda806c8a98558fe7af1c35840a
byplayer <byplayer100@gmail.com>
2020-03-02 10:55:08 +0900

    :+1: Activate updatedb cron job again

--- recipes/_updatedb.rb
+++ recipes/_updatedb.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# cron 'updatedb' do
-#   minute '0'
-#   user 'root'
-#   command "#{node['updatedb']['command']} #{node['updatedb']['opts']} > /var/log/updatedb.log 2>&1"
-# end
+cron 'updatedb' do
+  minute '0'
+  user 'root'
+  command "#{node['updatedb']['command']} #{node['updatedb']['opts']} > /var/log/updatedb.log 2>&1"
+end
