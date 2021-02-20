91f80e05f34863e507804f73d925018adb21398a
byplayer <byplayer100@gmail.com>
2021-02-20 11:17:50 +0900

    :+1: Add sdkman and add exec permission for .private_git_conf.sh

--- recipes/_devenv.rb
+++ recipes/_devenv.rb
@@ -63,7 +63,6 @@ end
   .dir_colors
   .globalrc
   .prettierrc
-  .private_git_conf.sh
   .textlintrc
   .tmux.conf
   .zshenv
@@ -81,6 +80,25 @@ end
               git_private_signing_key: node['devenv']['git']['private_signing_key'],
               gpg_default_key: node['devenv']['gpg']['default_key']
     mode '0600'
+    action :create
+  end
+end
+
+%w[
+  .private_git_conf.sh
+].each do |name|
+  template "#{devenv_user_home}/#{name}" do
+    source "#{name}.erb"
+    owner node['devenv']['user']
+    group node['devenv']['group']
+    variables git_user_name: node['devenv']['git']['user_name'],
+              git_user_email: node['devenv']['git']['user_email'],
+              git_signing_key: node['devenv']['git']['signing_key'],
+              git_private_user_name: node['devenv']['git']['private_user_name'],
+              git_private_user_email: node['devenv']['git']['private_user_email'],
+              git_private_signing_key: node['devenv']['git']['private_signing_key'],
+              gpg_default_key: node['devenv']['gpg']['default_key']
+    mode '0755'
     action :create
   end
 end
@@ -120,4 +138,17 @@ end
 link "#{devenv_user_home}/.pyenv/bin" do
   to '/usr/local/opt/pyenv/bin'
   link_type :symbolic
+end
+
+# sdkman
+bash 'install sdkman' do
+  cwd devenv_user_home
+  user node['devenv']['user']
+  group node['devenv']['group']
+  environment({ 'HOME' => devenv_user_home })
+
+  code <<-EOH
+    curl -s "https://get.sdkman.io" | bash
+  EOH
+  not_if "test -f #{devenv_user_home}/.sdkman/bin/sdkman-init.sh"
 end
