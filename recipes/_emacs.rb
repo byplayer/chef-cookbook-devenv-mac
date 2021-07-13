5a3bba343ef8a35974e5f134c5b763baa1ff3e37
byplayer <byplayer100@gmail.com>
2021-07-13 11:07:26 +0900

    fix emacs path

--- recipes/_emacs.rb
+++ recipes/_emacs.rb
@@ -4,6 +4,6 @@ bash 'link emacs app' do
     if [ -s /Applications/Emacs.app ] ; then
       rm /Applications/Emacs.app
     fi
-    ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
+    ln -s /usr/local/opt/emacs-plus@28/Emacs.app /Applications
   EOH
 end
