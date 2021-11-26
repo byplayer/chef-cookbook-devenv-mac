a16f2d88f016152ecfd19b9104d23cc3d7c816c2
byplayer <byplayer100@gmail.com>
2021-11-27 00:29:00 +0900

    👍 add fond dir creation

--- recipes/_source-han-code-jp.rb
+++ recipes/_source-han-code-jp.rb
@@ -8,6 +8,14 @@ remote_file "#{Chef::Config['file_cache_path']}/#{cache_file_name}" do
   not_if "test -f #{Chef::Config['file_cache_path']}/#{cache_file_name}"
 end
 
+directory "#{devenv_user_home}/library/fonts" do
+    owner node['devenv']['user']
+    group node['devenv']['group']
+    mode '0755'
+    recursive true
+    action :create
+end
+
 bash "install source-han-code-jp #{node['source-han-code-jp']['version']}" do
   cwd Chef::Config['file_cache_path']
   code <<-CODE
