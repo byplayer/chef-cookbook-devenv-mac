4b0352a192787af754f902564ac0e53f97c96ccc
byplayer <byplayer100@gmail.com>
2024-01-09 17:35:40 +0900

    🐛 fix compile issue

--- recipes/_homebrew.rb
+++ recipes/_homebrew.rb
@@ -4,7 +4,7 @@ include_recipe 'homebrew::install_taps'
 include_recipe 'homebrew::install_formulas'
 
 node['homebrew']['casks'].each do |name|
-  install_cask(name) do
+  cask(name) do
     action :install
     install_cask false
   end
